98712e773f266e17ad8bcda009b60aa21f3af98d
antoprocc <procopioantonio02@gmail.com>
2024-06-28 18:47:18 +0200

    main di testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("inizio");
+
+        Regole regole = new Regole(10,10,2,4,2,2,2,
+                2,2,2,true,false);
+
+        System.out.println("regole impostate");
+
+
+        Tabellone tabellone=new Tabellone(regole);
+
+        System.out.println("tabellone creato");
+
+
+        Partita partita=Partita.getInstance(tabellone);
+
+        System.out.println("partita creata");
+
+        partita.avviaPartita();
+
     }
 }

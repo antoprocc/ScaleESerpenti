9d93474d6360c9a2bd4089b730afd705cc8e50b0
antoprocc <procopioantonio02@gmail.com>
2024-06-29 13:33:10 +0200

    fix logica

--- src/main/java/org/example/caselle/CasellaPesca.java
+++ src/main/java/org/example/caselle/CasellaPesca.java
@@ -6,51 +6,60 @@ import org.example.dadi.DadoStrategy;
 
 import java.util.Random;
 
-public class CasellaPesca extends Casella{
+public class CasellaPesca extends Casella {
 
-    private String tipo;
+    private final String tipo;
+    private String tipoPescato;
 
-    public CasellaPesca(int numeroCasella){
+    public CasellaPesca(int numeroCasella) {
         super(numeroCasella);
-        tipo= "sosta";
+        this.tipo = "pesca una carta";
+        this.tipoPescato = "";
     }
 
     @Override
     public void effetto(Giocatore giocatore, DadoStrategy dadoStrategy, int traguardo, int passi, Tabellone tabellone) {
         Random random = new Random();
         int carta = random.nextInt(0,4);
-        switch (carta){
-            case 0: //panchina
-                giocatore.setTurniDaSaltare(giocatore.getTurniDaSaltare()+1);
-                System.out.println("il giocatore "+giocatore.getNome()+" è finito su una casella pesca una carta...Ha pescato: carta panchina");
+        switch (carta) {
+            case 0: // panchina
+                tipoPescato = "panchina";
+                System.out.println("... Ha pescato: carta " + tipoPescato);
+                giocatore.setTurniDaSaltare(giocatore.getTurniDaSaltare() + 1);
                 break;
-            case 1: //locanda
-                giocatore.setTurniDaSaltare(giocatore.getTurniDaSaltare()+3);
-                System.out.println("il giocatore "+giocatore.getNome()+" è finito su una casella pesca una carta...Ha pescato: carta locanda");
+            case 1: // locanda
+                tipoPescato = "locanda";
+                System.out.println("... Ha pescato: carta " + tipoPescato);
+                giocatore.setTurniDaSaltare(giocatore.getTurniDaSaltare() + 3);
                 break;
-            case 2: //dadi
+            case 2: // dadi
+                tipoPescato = "dadi";
+                System.out.println("... Ha pescato: carta " + tipoPescato);
                 int passiDaFare;
-
                 do {
                     passiDaFare = dadoStrategy.lancia();
                     muoviGiocatore(giocatore, passiDaFare, traguardo, dadoStrategy, tabellone);
                 } while (passiDaFare == 12);
-                System.out.println("il giocatore "+giocatore.getNome()+" è finito su una casella pesca una carta...Ha pescato: carta dadi");
                 break;
-            case 3: //molla
+            case 3: // molla
+                tipoPescato = "molla";
+                System.out.println("... Ha pescato: carta " + tipoPescato);
                 muoviGiocatore(giocatore, passi, traguardo, dadoStrategy, tabellone);
-                System.out.println("il giocatore "+giocatore.getNome()+" è finito su una casella pesca una carta...Ha pescato: carta molla");
                 break;
         }
     }
 
     private void muoviGiocatore(Giocatore giocatore, int passi, int traguardo, DadoStrategy dadoStrategy, Tabellone tabellone) {
-        giocatore.muovi(passi, traguardo,tabellone);
-        giocatore.getCasella().effetto(giocatore,dadoStrategy, traguardo, passi, tabellone);
+        giocatore.muovi(passi, traguardo, tabellone);
+        giocatore.getCasella().effetto(giocatore, dadoStrategy, traguardo, passi, tabellone);
     }
 
     public String getTipo() {
         return tipo;
     }
-}
 
+    @Override
+    public String toString() {
+        return tipo ;
+    }
+}
